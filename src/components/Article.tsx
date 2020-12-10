b9f2c2aa9201b931ca4a118c73dfa7623d4d590e
Aminessid <essidmohamedamin@gmail.com>
2020-12-10 20:11:07 +0100

    fix style

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,47 +1,46 @@
-import React from 'react';
-import Image from 'gatsby-image';
-import Paper from '@material-ui/core/Paper';
-import {Link} from "gatsby";
-
-
+import React from "react"
+import Image from "gatsby-image"
+import Paper from "@material-ui/core/Paper"
+import { Link } from "gatsby"
 
 type Props = {
-    post: {
-        article: {
-            article: string;
-        };
-        description: {
-            description: string;
-        };
-        contentful_id: string;
-        title: string;
-        mainImage: {
-            fluid: any;
-        };
-        smallImage: {
-            fluid: any;
-        };
-        slug: string;
+  post: {
+    article: {
+      article: string
     }
+    description: {
+      description: string
+    }
+    contentful_id: string
+    title: string
+    mainImage: {
+      fluid: any
+    }
+    smallImage: {
+      fluid: any
+    }
+    slug: string
+  }
 }
 
-const Article: React.FC<Props> = ({post}) => {
-    const { smallImage: {fluid : demoImage}, title, slug } = post
-    return (
-        <>
-            <Link to={`/${slug}`}>
-                <Paper elevation={2} className='post'>
-                    <p className="post-title">
-                        {title}
-                    </p>
-                    <div className="post-img">
-                        <Image fluid={demoImage} /> 
-                    </div>
-                </Paper>
-            </Link>
-        </>
-    )
+const Article: React.FC<Props> = ({ post }) => {
+  const {
+    smallImage: { fluid: demoImage },
+    title,
+    slug,
+  } = post
+  return (
+    <>
+      <Link to={`/${slug}`}>
+        <Paper elevation={2} className="post">
+          <div className="post-img">
+            <Image fluid={demoImage} />
+          </div>
+          <p className="post-title">{title}</p>
+        </Paper>
+      </Link>
+    </>
+  )
 }
 
 export default Article
-
